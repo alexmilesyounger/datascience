1739952777fb4cec6edc15eac59aa260795b9fcf
Alex Miles Younger <alex@alexmilesyounger.com>
2015-06-17 11:46:15 -0400

    Fix for bug with appending users to friend list

--- ex01.py
+++ ex01.py
@@ -26,3 +26,37 @@ friendships = [
 	(8, 9), 
 ]
 
+# add friends list to users
+for user in users:
+	user['friends'] = []
+
+# add friends to all users friends list
+for i, j in friendships:
+	users[i]['friends'].append(j) # add i as a friend of j
+	users[j]['friends'].append(i) # add j as a friend of i
+
+# find the total number of friends someone has
+def number_of_friends(user):
+	""" How many friends does _user_ have? """
+	return len(user['friends'])	# length of friend_ids list
+
+# find the total number of connections in our network
+total_connections = sum(number_of_friends(user) for user in users)
+
+
+####################################################################
+# NOTES ############################################################
+####################################################################
+
+# # return len(user(['friends'])) 	# length of friend_ids list
+# 	# return len(users[0]['friends']) # length of friends list for users[0]
+# 	return len(users[user_id]['friends']) # length of friends list
+# 	print len(users[i]['friends'])
+# 	return len(user(['friends'])) 	# length of friend_ids list
+
+# for user in users:
+# 	user_id = users['id']
+# 	print number_of_friends(user_ie)
+
+# total_connections = sum(number_of_friends(user) for user in users) #24
+# print total_connections
